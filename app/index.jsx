048468fffa52146a029b54a0f41277f60f34ff02
Johannes Norheim <johannes.j.norheim@gmail.com>
2016-02-14 17:57:28 -0500

    app self built components

---
+++ app/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render } from 'react-dom';
+import brace from 'brace';
+import AceEditor from 'react-ace';
+import AwesomeComponent from './testComponent.jsx';
+
+import 'brace/mode/java';
+import 'brace/theme/github';
+ 
+function onChange(newValue) {
+  console.log('changed it',newValue);
+}
+ 
+// Render editor 
+render(
+	<div>
+	  <AwesomeComponent/>
+	  <AceEditor
+	    mode="java"
+	    theme="github"
+	    onChange={onChange}
+	    name="UNIQUE_ID_OF_DIV"
+	    editorProps={{$blockScrolling: true}}
+	  />
+	 </div>,
+  document.getElementById('example2')
+);
+
+if (module.hot) {
+  module.hot.accept();
+}

d6dec27df57e93a4928b022b7b7214e3464f8849
Johannes Norheim <johannes.j.norheim@gmail.com>
2016-02-14 18:18:19 -0500

    fixed reload error, still have hot reload problem

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ var webpack = require('webpack');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
 var config = require('./webpack.config');
-var compiler = webpack(config);
 
 var app = new require('express')();
 var port = 3000;
 
+var compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, { 
-		filename: 'bundle.js',
+		noInfo: true,
 		hot: true,
 		publicPath: config.output.publicPath
 }));

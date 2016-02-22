072a3dc1961e90e3dc7fb4b9e9173aabf10464a1
Johannes Norheim <johannes.j.norheim@gmail.com>
2016-02-22 12:48:57 -0500

    added socket communication

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 import brace from 'brace';
+
 import AceEditor from 'react-ace';
 import AwesomeComponent from './testComponent.jsx';
 
 import 'brace/mode/java';
 import 'brace/theme/github';
 import 'brace/theme/monokai';
+
+import io from 'socket.io-client';
+let socketPort = 3001;
+let socket = io('http://localhost:' + socketPort.toString());
+socket.on('connect', function(){console.log('connected')});
+socket.emit('client:sendMessage', 'hello');
 
 function onChange (newValue){
 	console.log(newValue);
--- server.js
+++ server.js
@@ -3,9 +3,16 @@ var webpack = require('webpack');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
 var config = require('./webpack.config');
-
 var app = new require('express')();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var port = 3000;
+var socketPort = 3001;
+
+http.listen(socketPort, function(){
+  console.log('listening on *:' + socketPort.toString());
+});
 
 var compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, { 
@@ -27,3 +34,14 @@ app.listen(port, function(error) {
     console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port);
   }
 });
+
+
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('client:sendMessage', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
